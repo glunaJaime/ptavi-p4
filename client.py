209c4acd882467b57394980b0dccf38d641af040
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-31 12:04:04 +0100

    client.py 6 terminado

--- client.py
+++ client.py
@@ -5,22 +5,35 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
 #SERVER = 'localhost'
 #PORT = 6001
 #LINE = '¡Hola mundo!'
 
+def registro(mensaje, direccion):
+    palabra = "REGISTER"
+    palabra = palabra + "sip: " + direccion + "SIP/2.0\r\n\r\n" + mensaje
+    print(palabra)
+    return palabra
+    
+# Constantes. Dirección IP del servidor y contenido a enviar
 server = sys.argv[1]
 port = int(sys.argv[2])
-line = (' '.join)(sys.argv[3:])
+#line = (' '.join)(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsocket(socket.SOL_SOKET, soket.SOCK_REUSADDR, 1)
+my_socket.connect(server, port)
+
+frase = ' '.join(line)
+print("Enviando: " + frase)
+my_socket.send(bytes(frase, 'utf-8') + b'\r\n')
+
+my_socket.send(bytes(registro(frase, mi_direc), 'utf-8') + '\r\n')
+
+data = my_socket.recv(1024)
+print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
