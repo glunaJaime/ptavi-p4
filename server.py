c3ae935d66f7708c5528a58d43f959e28c14b869
Jaime Garcia Luna <jluna@alpha09.aulas.gsyc.urjc.es>
2018-10-31 17:26:38 +0100

    server.py pep8 y pep257

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import sys
 import socketserver
