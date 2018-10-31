f23622f44725f8088d8483f12b97894239c46d34
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-31 17:13:10 +0100

    client.py pep8

--- client.py
+++ client.py
@@ -7,34 +7,25 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-#SERVER = 'localhost'
-#PORT = 6001
-#LINE = '¡Hola mundo!'
-
-def registro(mi_direccion, t_exp):
-    palabra = "REGISTER"
-    palabra = palabra + "sip: " + direccion + "SIP/2.0\r\n\r\n" + mensaje
-    print(palabra)
-    return palabra
 try:
-    # Constantes. Dirección IP del servidor y contenido a enviar
-    server = sys.argv[1]
-    port = int(sys.argv[2])
-    tipo_mensaje = sys.argv[3:]
-    mi_direc = sys.argv[4]
-    time_expires = sys.argv[5]
-    line = sys.argv[3:]
-    # line=(' '.join)(sys.argv[3:])
-     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((server, port))
-     # frase = ' '.join(line)
-    # print("Enviando:" + frase)
-    # my_socket.send(bytes(frase, 'utf-8') + b'\r\n')
-     my_socket.send(bytes(registro(mi_direc, t_exp), 'utf-8') + b'\r\n')
+    SERVER = str(sys.argv[1])
+    PORT = int(sys.argv[2])
+    line = str(' '.join(sys.argv[3:5]))
+    expires_value = sys.argv[5]
+except:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value.")
+
+chops = line.split(' ')
+sip_str = ' SIP/2.0\r\n'
+expires_str = 'Expires: ' + expires_value + '\r\n\r\n'
+if sys.argv[3] == 'register':
+    line = str(chops[0].upper() + ' sip:' + chops[1] + sip_str + expires_str)
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    my_socket.connect((SERVER, PORT))
+    print("Enviando:" + '\r\n' + line)
+    my_socket.send(bytes(line, 'utf-8'))
     data = my_socket.recv(1024)
-     print('Recibido -- ', data.decode('utf-8'))
-    print("Socket terminado.")
-except IndexError:
-    print("Usage: client.py ip puerto register sip_address expires_value")
+    print('Recibido -- ', data.decode('utf-8'))
+
+print("Socket terminado.")
