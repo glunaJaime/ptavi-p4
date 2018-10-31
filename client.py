db0ca31dae4e5ba7265f4cb75b514090d736bbaf
Jaime Garcia <luna@MacBook-Pro-de-Jaime.local>
2018-10-31 11:36:09 +0100

    empezando client.py

--- client.py
+++ client.py
@@ -7,9 +7,13 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+#SERVER = 'localhost'
+#PORT = 6001
+#LINE = '¡Hola mundo!'
+
+server = sys.argv[1]
+port = int(sys.argv[2])
+line = (' '.join)(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
